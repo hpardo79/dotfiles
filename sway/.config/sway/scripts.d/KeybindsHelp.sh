d5d2f8465cb5243efd2977c4dabc1ee83c869c57
hpardo79 <kektoreon.0679@gmx.com>
2024-07-28 21:47:06 -0500

    update sway config

--- sway/.config/sway/scripts.d/KeybindsHelp.sh
+++ sway/.config/sway/scripts.d/KeybindsHelp.sh
@@ -20,38 +20,36 @@ Yellow="#F1FA8C" #(a bright yellow color)
 ##################################################
 
 # My keybinds
-CONTENT="<span face='monospace' foreground='$Yellow'> 
-<big><b><span foreground='$Orange'>My Keybinds Helps</span></b></big>
+CONTENT="<span face='monospace' foreground='$Yellow'><b><span foreground='$Orange'>My Keybinds Helps</span></b>
 
 <b><span foreground='$Purple'>Manjaro Sway Keybinds</span></b>
-<b><span foreground='$Green'>Mod  +  Shift  +  e    </span>= Shutdown Mode </b>
-<b><span foreground='$Green'>Mod  +  Shift  +  c    </span>= Sway Reload </b>
-<b><span foreground='$Green'>Mod  +  Shift  +  q    </span>= Kill (Apps) 󰯆</b>
-<b><span foreground='$Green'>Mod  +  Shift  +  b    </span>= Hide/Unhide Waybar 󰈉</b>
-<b><span foreground='$Green'>Mod  +  Return         </span>= Terminal Foo </b>
-<b><span foreground='$Green'>Mod  +  d              </span>= Menu (*Wofi) 󰀻</b>
-<b><span foreground='$Green'>Ctrl +  Alt    +  Del  </span>= Task Manager </b>
-<b><span foreground='$Green'>Mod  +  Shift  +  i    </span>= Inhibit-Idle Interactive 󰒳</b>
-<b><span foreground='$Green'>Mod  +  r              </span>= Resize Mode 󰲏</b>
-<b><span foreground='$Green'>Mod  +  Shift  +  p    </span>= Clipboard </b>
-<b><span foreground='$Green'>Mod  +  ← ↑ → ↓        </span>= Focus 󰼀</b>
-<b><span foreground='$Green'>Mod  +  1 > 9          </span>= Select Workspace </b>
+<small><span foreground='$Green'>Mod  +  Shift  +  e    </span> = Shutdown Mode 
+<span foreground='$Green'>Mod  +  Shift  +  c    </span> = Sway Reload 
+<span foreground='$Green'>Mod  +  Shift  +  q    </span> = Kill (Apps) 󰯆
+<span foreground='$Green'>Mod  +  Shift  +  b    </span> = Hide/Unhide Waybar 󰈉
+<span foreground='$Green'>Mod  +  Return         </span> = Terminal Foo 
+<span foreground='$Green'>Mod  +  d              </span> = Menu (*Wofi) 󰀻
+<span foreground='$Green'>Ctrl +  Alt    +  Del  </span> = Task Manager 
+<span foreground='$Green'>Mod  +  Shift  +  i    </span> = Inhibit-Idle 󰒳
+<span foreground='$Green'>Mod  +  r              </span> = Resize Mode 󰲏
+<span foreground='$Green'>Mod  +  Shift  +  p    </span> = Clipboard 
+<span foreground='$Green'>Mod  +  ← ↑ → ↓        </span> = Focus 󰼀
+<span foreground='$Green'>Mod  +  1 > 9          </span> = Select Workspace </small>
 
 <b><span foreground='$Purple'>My Keybinds</span></b>
-<b><span foreground='$Green'>Ctrl +  Shift  +  h    </span>= Keybinds Help 󰞋</b>
-<b><span foreground='$Green'>Mod  +  Ctrl   +  p    </span>= PowerMenu Mode </b>
-<b><span foreground='$Green'>Ctrl +  Return         </span>= Kitty Terminal </b>
-<b><span foreground='$Green'>Ctrl +  Shift  +  b    </span>= Brave Browser 󰖟</b>
-<b><span foreground='$Green'>Ctrl +  Shift  +  f    </span>= Firefox Browser 󰈹</b>
-<b><span foreground='$Green'>Ctrl +  Alt    +  f    </span>= Yazi File Manager 󰇥</b>
-<b><span foreground='$Green'>Ctrl +  Shift  +  t    </span>= Thunar File Manager </b>
-<b><span foreground='$Green'>Ctrl +  Shift  +  m    </span>= Mousepad Editor 󱩼</b>
+<small><span foreground='$Green'>Ctrl +  Shift  +  h    </span> = Keybinds Help 󰞋
+<span foreground='$Green'>Mod  +  Ctrl   +  p    </span> = PowerMenu Mode 
+<span foreground='$Green'>Ctrl +  Return         </span> = Kitty Terminal 
+<span foreground='$Green'>Ctrl +  Shift  +  b    </span> = Brave Browser 󰖟
+<span foreground='$Green'>Ctrl +  Shift  +  f    </span> = Firefox Browser 󰈹
+<span foreground='$Green'>Mod  +  Ctrl   +  f    </span> = Yazi File Manager 󰇥
+<span foreground='$Green'>Ctrl +  Shift  +  t    </span> = Thunar File Manager 
+<span foreground='$Green'>Ctrl +  Shift  +  m    </span> = Mousepad Editor 󱩼</small>
 
-<big><b><span foreground='$Red'>󱞩 Keep It Simple Stupid 󱞥</span></b></big>
-</span>
+<b><span foreground='$Red'>󱞩 Keep It Simple Stupid 󱞥</span></b></span>
 "
 
 # Run
-keyhelp=$(yad --title="Keybinds Help" --text="$CONTENT" --no-buttons --close-on-unfocus)
+keyhelp=$(yad --title="Keybinds Help" --text="$CONTENT" --borders=10 --no-buttons --close-on-unfocus)
 
-swaymsg exec '$keyhelp' 
+swaymsg exec '$keyhelp'
